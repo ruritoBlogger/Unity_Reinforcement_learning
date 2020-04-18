50b6f1c2303075d542383f2e9cd9e6104d53039f
rurito <rurito.blog@gmail.com>
2020-04-18 16:41:08 +0900

    ボールと棒を操作する部分を実装した

---
+++ Assets/BallController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10;
+    void Start()
+    {
+        var force = ( transform.forward + transform.right ) * speed;
+        GetComponent<Rigidbody>().AddForce( force, ForceMode.VelocityChange );  
+    }
+}
