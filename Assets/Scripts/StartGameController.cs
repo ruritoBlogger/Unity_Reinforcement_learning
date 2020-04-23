dae0fc752aaffc5cfc541ea1e19ccedcdc880010
rurito <rurito.blog@gmail.com>
2020-04-23 22:01:12 +0900

    スタート画面のテンプレを実装

---
+++ Assets/Scripts/StartGameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameController : MonoBehaviour
+{
+    // PlayGameがクリックされたときゲームを実行する
+    public void OnClick()
+    {
+        SceneManager.LoadScene("PlayGame");
+    }
+}
