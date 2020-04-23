92ae5297893d6e3cf3bc46318709ccd77e21bb00
rurito <rurito.blog@gmail.com>
2020-04-23 19:56:02 +0900

    ブロックの数に連動してUIを表示する機能を実装

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,14 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 10;
+    
+    // 残っているブロックの数
+    public int blocks;
+
+    // ブロックに設定しているtagの名前
+    private string tag_name = "enemy";
+
+    // 残っているブロックの数を画面に表示する
+    public Text scoreText;
+
     void Start()
     {
+        // 初動の設定
         var force = ( transform.forward + transform.right ) * speed;
-        GetComponent<Rigidbody>().AddForce( force, ForceMode.VelocityChange );  
+        GetComponent<Rigidbody>().AddForce( force, ForceMode.VelocityChange );
+
+        // ブロックの総数を数える
+        GameObject[] tagObjects = GameObject.FindGameObjectsWithTag(tag_name);
+        blocks = tagObjects.Length; 
+            
+        SetUI();
+        Debug.Log("FUCK UNCHI!!!l");
+    }
+
+    // ブロックと玉が衝突した場合、表示している数値を減らす
+    void OnTriggerEnter(Collider other)
+    {
+        if( other.gameObject.CompareTag(tag_name))
+        {
+            blocks--;
+            other.gameObject.SetActive(false);
+            SetUI();
+            Debug.Log("test");
+        }
+        else
+        {
+            Debug.Log("FUCK UNCHI!!!l");
+        }
+    }
+
+    // UIの設定
+    void SetUI()
+    {
+        scoreText.text = "Count: " + blocks.ToString();
     }
 }
