a24fc8968b81b4ecc5e22fdee594a12ad3d62a33
rurito <rurito.blog@gmail.com>
2020-05-14 09:57:33 +0900

    PlayGameモードにメニューに戻る機能を実装

--- Assets/Scripts/ReturnButtonBehavier.cs
+++ Assets/Scripts/ReturnButtonBehavier.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonBehavier : MonoBehaviour
+public class ReturnButtonBehavier : MonoBehaviour
 {
     public void OnClick()
     {
